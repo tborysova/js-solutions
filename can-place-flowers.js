6c46377c6e73b6e1e3cca837b86afcd37856339b
tborysova <borysova.tetiana@gmail.com>
2023-03-20 15:27:18 +0100

    Can Place Flowers

---
+++ can-place-flowers.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+  let count = 0;
+  let i = 0;
+  while (i < flowerbed.length) {
+    if (
+      flowerbed[i] === 0 &&
+      (i === 0 || flowerbed[i - 1] === 0) &&
+      (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+    ) {
+      flowerbed[i] = 1;
+      count++;
+    }
+    i++;
+  }
+  return count >= n;
+};
